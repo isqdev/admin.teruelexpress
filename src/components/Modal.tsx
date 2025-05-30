da594186fd18fcfea3f21d11d8923e99769e063a
Isaque Dias <isaque.brain@gmail.com>
2025-05-30 12:42:50 -0300

    quickfix(Modal): ajusta o modal da versao mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,8 +13,8 @@ export function Modal({ className, children, data, open = true, onClose, ...prop
     if (!open) return null; 
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30" onClick={onClose}>
-            <div className={twMerge("p-6 min-h-screen sm:min-h-0 sm:max-w-3xl w-full sm:aspect-[4/3]  mx-auto rounded-2xl bg-gray-50", className)} {...props}>
+        <div className="fixed inset-0 z-50 flex sm:items-center sm:justify-center bg-black/30" onClick={onClose}>
+            <div className={twMerge("p-6 min-h-screen sm:min-h-0 sm:max-w-3xl w-full sm:aspect-[4/3] rounded-2xl bg-white", className)} {...props}>
                 <div>
                     {children ? children : "--vazio--"}
                 </div>
