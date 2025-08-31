f267623d32aafbb4e859bd31ff99e0b5d6666151
Gregory Soares <gregory16704@gmail.com>
2025-08-31 01:25:41 -0300

    hotfix: corrigin import

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL,
@@ -10,7 +11,7 @@ const api = axios.create({
 api.interceptors.request.use(
     config => {
         const token = Cookies.get('token');
-        if (usuario) {
+        if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
         return config;
